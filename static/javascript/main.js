21f91cc5674efd33267240f6de4a9010c47d164f
Igor_Ribeiro <ribeiro_igor@hotmail.com>
2016-09-27 00:03:56 -0300

    Added bar for likes and dislikes

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -4,9 +4,20 @@ var thumbsDown = 0;
 function increment() {
     thumbsUp ++;
     document.getElementById("thumbsUp").innerHTML = thumbsUp;
+    calculateBar();
 }
 
 function decrease() {
     thumbsDown ++;
     document.getElementById("thumbsDown").innerHTML = thumbsDown;
+    calculateBar();
+}
+
+function calculateBar() {
+    var total = thumbsDown + thumbsUp;
+    var percentageThumbsUp = thumbsUp/total*100;
+    var percentageThumbsDown = thumbsDown/total*100;
+
+    document.getElementById("likes").style.width = percentageThumbsUp.toString()+"%";
+    document.getElementById("dislikes").style.width = percentageThumbsDown.toString()+"%";
 }
