25e7aea886154ca1b3be85af8cbd3b46878116ab
Igor_Ribeiro <ribeiro_igor@hotmail.com>
2016-09-29 22:46:35 -0300

    Now the form work on enter press key!

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,9 +1,10 @@
-window.onload = function() {
-    document.getElementById("search-icon").onclick = function () {
+document.getElementById("inputInfo").onkeydown = function (e) {
+    if(e.keyCode === 13) {
         var input = document.getElementById('inputInfo').value;
-        input = input.toLowerCase();
+
         if(input==="10 Porcento" || input==="10 porcento")
             location.href = "pages/10_porcento.html";
+
         else if (input==="Amiga Parceira" || input==="Amiga parceira" || input==="amiga parceira" || input==="amiga Parceira")
             location.href="pages/amiga_parceira.html";
 
@@ -33,4 +34,40 @@ window.onload = function() {
         else
             location.href = "video_not_found.html";
     }
-};
+}
+
+document.getElementById("search-icon").onclick = function () {
+    var input = document.getElementById('inputInfo').value;
+
+    if(input==="10 Porcento" || input==="10 porcento")
+        location.href = "pages/10_porcento.html";
+
+    else if (input==="Amiga Parceira" || input==="Amiga parceira" || input==="amiga parceira" || input==="amiga Parceira")
+        location.href="pages/amiga_parceira.html";
+
+    else if (input ==="Baile de Favela" || input==="baile de favela")
+        location.href="pages/baile_de_favela.html"
+
+    else if (input ==="Bumbum Granada" || input==="bumbum granada")
+        location.href="pages/bumbum_granada.html"
+
+    else if (input ==="Chefe é chefe né Pai" || input==="chefe é chefe né pai" || input==="chefe e chefe ne pai")
+        location.href="pages/chefe_e_chefe_ne_pai.html"
+
+    else if (input ==="Devagarinho" || input==="devagarinho")
+        location.href="pages/devagarinho.html"
+
+    else if (input ==="Fetiche" || input==="fetiche")
+        location.href="pages/fetiche.html"
+
+    else if (input ==="Novinha vai no chão" || input==="novinha vai no chão" || input==="novinha vai no chao")
+        location.href="pages/novinha_vai_no_chão.html"
+
+    else if (input ==="Quando o DJ mandar" || input==="quando o DJ mandar" || input==="quando o dj mandar")
+        location.href="pages/quando_o_DJ_mandar.html"
+
+    else if(input==="Vem pro meu lounge" || input==="vem pro meu lounge")
+        location.href="pages/vem_pro_meu_lounge.html"
+    else
+        location.href = "video_not_found.html";
+}
--- static/javascript/search.js
+++ static/javascript/search.js
@@ -1,9 +1,10 @@
-window.onload = function() {
-    document.getElementById("search-icon").onclick = function () {
+document.getElementById("inputInfo").onkeydown = function (e) {
+    if(e.keyCode === 13) {
         var input = document.getElementById('inputInfo').value;
-        input = input.toLowerCase();
+
         if(input==="10 Porcento" || input==="10 porcento")
             location.href = "10_porcento.html";
+
         else if (input==="Amiga Parceira" || input==="Amiga parceira" || input==="amiga parceira" || input==="amiga Parceira")
             location.href="amiga_parceira.html";
 
@@ -33,4 +34,39 @@ window.onload = function() {
         else
             location.href = "../video_not_found.html";
     }
-};
+}
+
+document.getElementById("search-icon").onclick = function () {
+    var input = document.getElementById('inputInfo').value;
+    input = input.toLowerCase();
+    if(input==="10 Porcento" || input==="10 porcento")
+        location.href = "10_porcento.html";
+    else if (input==="Amiga Parceira" || input==="Amiga parceira" || input==="amiga parceira" || input==="amiga Parceira")
+        location.href="amiga_parceira.html";
+
+    else if (input ==="Baile de Favela" || input==="baile de favela")
+        location.href="baile_de_favela.html"
+
+    else if (input ==="Bumbum Granada" || input==="bumbum granada")
+        location.href="bumbum_granada.html"
+
+    else if (input ==="Chefe é chefe né Pai" || input==="chefe é chefe né pai" || input==="chefe e chefe ne pai")
+        location.href="chefe_e_chefe_ne_pai.html"
+
+    else if (input ==="Devagarinho" || input==="devagarinho")
+        location.href="devagarinho.html"
+
+    else if (input ==="Fetiche" || input==="fetiche")
+        location.href="fetiche.html"
+
+    else if (input ==="Novinha vai no chão" || input==="novinha vai no chão" || input==="novinha vai no chao")
+        location.href="novinha_vai_no_chão.html"
+
+    else if (input ==="Quando o DJ mandar" || input==="quando o DJ mandar" || input==="quando o dj mandar")
+        location.href="quando_o_DJ_mandar.html"
+
+    else if(input==="Vem pro meu lounge" || input==="vem pro meu lounge")
+        location.href="vem_pro_meu_lounge.html"
+    else
+        location.href = "../video_not_found.html";
+}
